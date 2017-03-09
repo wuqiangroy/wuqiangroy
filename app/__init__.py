594f638a3bd61e07364a9abe95475ec019c2c579
wuqiangroy <wuqiangroy@live.com>
2017-03-09 23:14:55 +0800

    add app

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python
 # _*_ coding:utf-8 _*_
+
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__)
+
+    from .auth import auth
+    app.register_blueprint(auth)
