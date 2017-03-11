8a7e95f0495be8ff92e5880961406faa6389adc0
wuqiangroy <wuqiangroy@live.com>
2017-03-11 19:43:46 +0800

    add multi threading

--- app/auth/util.py
+++ app/auth/util.py
@@ -2,7 +2,17 @@
 # _*_ coding:utf-8 _*_
 
 import smtplib
+import threading
+
+
+class ThreadFun(threading.Thread):
+    """执行多线程"""
+
+    def __init__(self):
+        super(ThreadFun, self).__init__()
+
 
 
 class SendMail(object):
     """send mail server"""
+
