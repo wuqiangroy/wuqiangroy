afaa877987774b1fb7741310f20f2cb87dcdd45e
wuqiangroy <wuqiangroy@live.com>
2017-03-11 19:44:49 +0800

    add config

--- config.py
+++ config.py
@@ -1,2 +1,12 @@
 #!/usr/bin/env python
 # _*_ coding:utf-8 _*_
+
+"""配置"""
+
+import os
+import sys
+
+
+class Config(object):
+    SECRET_KEY = os.environ.get("secret_key") or "easy to guess secret key"
+
