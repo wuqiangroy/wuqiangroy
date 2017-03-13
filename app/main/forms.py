c9812632922f20505768d363d6fe818806e6c1ec
wuqiangroy <wuqiangroy@live.com>
2017-03-13 23:23:02 +0800

    add main/forms

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python
 # _*_ coding:utf-8 _*_
+
+from wtforms import Form, StringField, SelectField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class ProductForms(Form):
+    """商品表单"""
+
+    name = StringField(u"商品名", validators=[DataRequired()])
+    category = SelectField(u"选择种类", coerce=int)
+    introduction = TextAreaField(u"简介")
+    submit = SubmitField(u"发布")
