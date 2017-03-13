771912d02423d11cfa163abbf315987632ae344e
wuqiangroy <wuqiangroy@live.com>
2017-03-13 23:19:43 +0800

    add decorator

---
+++ app/decorator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+
+from functools import wraps
+
+from flask import abort
+from flask_login import current_user
+
+
+def permission_required(permission):
+
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            if not current_user.can(permission):
+                abort(403)
+            return f(*args, **kwargs)
+        return decorated_function
+    return decorator
+
+
