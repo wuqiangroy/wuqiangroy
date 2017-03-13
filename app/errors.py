65b016db650feba178246843cec5b4f81c7aa43a
wuqiangroy <wuqiangroy@live.com>
2017-03-13 23:20:29 +0800

    add errors

--- app/errors.py
+++ app/errors.py
@@ -1,2 +1,15 @@
 #!/usr/bin/env python
 # _*_ coding:utf-8 _*_
+
+
+class Errors(object):
+    """错误提示"""
+    SUCCESS = 1200
+    NO_DATA = 1001
+    SERVER_ERROR = 1002
+    # waiting for ......
+    msg = {
+        SUCCESS: u"成功",
+        NO_DATA: u"未传递数据",
+        SERVER_ERROR: u"服务器错误"
+    }
