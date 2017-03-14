7ec53d25ebd292ca162a8711cfcc670a7193e8de
wuqiang <wuqiang@wolongdata.com>
2017-03-14 20:21:20 +0800

    add new app

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,27 @@
 # _*_ coding:utf-8 _*_
 
 from flask import Flask
+from flask_mail import Mail
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+
+from config import config
+
+mail = Mail()
+login_manager = LoginManager()
+db = SQLAlchemy()
 
 
-def create_app():
+def create_app(config_name):
     app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    mail.init_app(app)
+    login_manager.init_app(app)
+    db.init_app(app)
 
     from .auth import auth
     app.register_blueprint(auth)
+    from .main import main
+    app.register_blueprint(main)
