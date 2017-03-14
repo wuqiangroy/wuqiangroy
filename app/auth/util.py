f45e5d0011fc66f32bbb8bf09d74f2b46e459592
wuqiang <wuqiang@wolongdata.com>
2017-03-14 20:21:56 +0800

    add send mail function

--- app/auth/util.py
+++ app/auth/util.py
@@ -4,15 +4,42 @@
 import smtplib
 import threading
 
-
-class ThreadFun(threading.Thread):
-    """执行多线程"""
-
-    def __init__(self):
-        super(ThreadFun, self).__init__()
+from flask import current_app, render_template
+from flask_mail import Message
+from .. import mail
 
 
 
-class SendMail(object):
-    """send mail server"""
+class ThreadFun(object):
+    """multi threading server"""
+
+    def __init__(self, func, args):
+        self.func = func
+        self.args = args
+
+    def __call__(self):
+        self.res = self.func(self.args)
+
+
+def send_async_mail(app, msg):
+    """multithreading send mail"""
+
+    with app.app_context():
+        mail.send(msg)
+
+
+def send_mail(to, subject, template, **kwargs):
+    """send mail function"""
+
+    app = current_app._get_current_object()
+    msg = Message(app.config["MAIL_SUBJECT_PREFIX"]+" "+subject,
+                  sender=app.config["MAIL_SENDER"], recipients=[to])
+    msg.body = render_template(template+".txt", **kwargs)
+    msg.html = render_template(template+".html", **kwargs)
+
+    thr = threading.Thread(target=ThreadFun(send_async_mail, (app, msg)))
+    thr.start()
+    return thr
+
+
 
