260d131e83bde9d3b7de003b1676d6d53bb1165d
wuqiangroy <wuqiangroy@live.com>
2017-03-14 22:24:50 +0800

    add running setting

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 # _*_ coding:utf-8 _*_
 
-from .app import create_app
+import os
 
-app = create_app()
+from .app import create_app, db
+from flask_script import Manager
+
+app = create_app(os.getenv("config") or "default")
+manager = Manager(app)
 
 if __name__ == "__main__":
-    app.run("localhost", port=8000, debug=True)
+    app.run()
 
