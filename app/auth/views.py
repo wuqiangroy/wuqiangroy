673de9591d8fc6dd3f2866b54b436a4c186121d6
wuqiangroy <wuqiangroy@live.com>
2017-03-17 23:57:27 +0800

    add some structers, and fulfill login

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 # _*_ coding:utf-8 _*_
 
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for, flash
+from flask_login import logout_user, login_required
 from . import auth
-from .forms import RegisterForm
+from .forms import RegisterForm, LoginForm
+from ..models import User
 
 
 @auth.route("/register/", methods=["GET", "POST"])
@@ -19,3 +21,26 @@ def register():
     password2 = form.password2.data.replace(" ", "")
 
     """发送邮件"""
+
+
+@auth.route("/login/", methods=["GET", "POST"])
+def login():
+    """user log in"""
+
+    form = LoginForm(request.args)
+    if form.validate():
+        username = form.username.data
+        user = User.query.filter_by(username=username).first()
+        if user is not None and user.verify_password(form.password.data):
+            # need session
+            return redirect(url_for("main.sweet_grass"))
+        flash(u"用户名或密码错误！")
+    return render_template("login.html", form=form)
+
+    
+@login_required
+@auth.route("/logout/")
+def logout():
+    """user logout, must be login status"""
+    logout_user()
+    return redirect(url_for("main.sweet_grass"))
